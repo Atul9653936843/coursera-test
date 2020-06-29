eb457ad01a7f4375b25aec3dd20b83871d128a8f
Atul9653936843 <66528224+Atul9653936843@users.noreply.github.com>
2020-06-29 12:33:34 +0530

    Delete SpeakGoodBye.js

--- md4/SpeakGoodBye.js
+++
@@ -1,9 +0,0 @@
-(function (window) {
-  var speakWord = 'Hello';
-  var byeSpeaker = {
-  speak : function(name){
-    console.log(speakWord + " " + name);
-  }}
-  window.byeSpeaker=byeSpeaker;
-
-})(window);
